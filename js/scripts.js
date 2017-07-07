d3c01bc28502789b750d733bcbd2916610db5859
Krzysztof Markowski <markowski@gdansk.home.pl>
2017-07-08 01:01:55 +0200

    Events

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
- 
+var add = document.getElementById('addElement');
+var list = document.getElementById('list');
+add.addEventListener('click', function(e) {
+  var element = document.createElement('li');
+  var howMuchElements = document.getElementsByTagName('li').length;
+  element.innerHTML = 'item ' + howMuchElements;
+  list.appendChild(element);
+});
